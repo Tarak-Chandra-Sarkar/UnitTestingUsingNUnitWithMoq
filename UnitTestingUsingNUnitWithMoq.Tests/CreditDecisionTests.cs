9db1519a26cd209c12e16ca280774a561bf30d27
Tarak-Chandra-Sarkar <tarak.chandra.sarkar@hotmail.com>
2019-05-31 21:49:11 +0530

    Test using NUnit with Moq frameworks

---
+++ UnitTestingUsingNUnitWithMoq.Tests/CreditDecisionTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTestingUsingNUnitWithMoq.Tests
+{
+    [TestFixture]
+    public class CreditDecisionTests
+    {
+        Mock<ICreditDecisionService> mockCreditDecisionService;
+
+        CreditDecision systemUnderTest;
+
+        [TestCase(100, "Declined")]
+        [TestCase(549, "Declined")]
+        [TestCase(550, "Maybe")]
+        [TestCase(674, "Maybe")]
+        [TestCase(675, "We look forward to doing business with you!")]
+        public void MakeCreditDecision_Always_ReturnsExpectedResult(int creditScore, string expectedResult)
+        {
+            mockCreditDecisionService = new Mock<ICreditDecisionService>(MockBehavior.Strict);
+            mockCreditDecisionService.Setup(p => p.GetCreditDecision(creditScore)).Returns(expectedResult);
+
+            systemUnderTest = new CreditDecision(mockCreditDecisionService.Object);
+            var result = systemUnderTest.MakeCreditDecision(creditScore);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+
+            mockCreditDecisionService.VerifyAll();
+        }
+    }
+}
